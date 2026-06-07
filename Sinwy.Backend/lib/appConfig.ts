887d89d88996346d0fe0634dc5678ca6805923f7
fraxx. <101990229+fraxxio@users.noreply.github.com>
2026-06-07 19:19:32 +0300

    Setup local db container

--- Sinwy.Backend/lib/appConfig.ts
+++ Sinwy.Backend/lib/appConfig.ts
@@ -8,11 +8,26 @@ const __dirname = dirname(__filename);
 
 config({ path: join(__dirname, "../.env") });
 
-const configSchema = z.object({
-	NODE_ENV: z.enum(["dev", "local", "prod"]),
-	PORT: z.coerce.number().default(3000),
-	DB_URL: z.url(),
-	BETTER_AUTH_SECRET: z.string(),
-});
+const configSchema = z
+	.object({
+		NODE_ENV: z.enum(["dev", "local", "prod"]),
+		PORT: z.coerce.number().default(3000),
+		BETTER_AUTH_SECRET: z.string(),
+		POSTGRES_USER: z.string(),
+		POSTGRES_PASSWORD: z.string(),
+		POSTGRES_DB: z.string(),
+		POSTGRES_HOST: z.string().default("localhost"),
+		POSTGRES_PORT: z.coerce.number().default(5432),
+	})
+	.transform((env) => {
+		const DB_URL =
+			`postgresql://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}` +
+			`@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`;
+
+		return {
+			...env,
+			DB_URL,
+		};
+	});
 
 export default configSchema.parse(process.env);
