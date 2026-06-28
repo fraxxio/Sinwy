d1563c13161a1e994962f6875e3d888c1656c73f
fraxx. <101990229+fraxxio@users.noreply.github.com>
2026-06-28 21:48:00 +0300

    (backend) set up initial user creation logic

--- Sinwy.Backend/modules/auth/auth.ts
+++ Sinwy.Backend/modules/auth/auth.ts
@@ -1,15 +1,47 @@
-// import db from "@db";
+import appConfig from "@config";
+import db from "@db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { organization } from "better-auth/plugins/organization";
-import db from "./../../db"; // Better auth doesn't support aliases
+import { sendEmail } from "./email";
 
 export const auth = betterAuth({
+	baseURL: appConfig.BETTER_AUTH_URL,
 	database: drizzleAdapter(db, {
 		provider: "pg",
 	}),
 	emailAndPassword: {
 		enabled: true,
+		requireEmailVerification: true,
 	},
-	plugins: [organization()],
+	emailVerification: {
+		sendVerificationEmail: async ({ user, url }) => {
+			await sendEmail({
+				to: user.email,
+				subject: "Verify your email",
+				html: `<p>Click <a href="${url}">here</a> to verify your email.</p>`,
+			});
+		},
+	},
+	socialProviders: {
+		google: {
+			clientId: appConfig.GOOGLE_CLIENT_ID,
+			clientSecret: appConfig.GOOGLE_CLIENT_SECRET,
+		},
+	},
+	plugins: [
+		organization({
+			schema: {
+				organization: {
+					additionalFields: {
+						status: {
+							type: "string",
+							defaultValue: "inactive",
+							input: false,
+						},
+					},
+				},
+			},
+		}),
+	],
 });
--- Sinwy.Shared/types/user/User.ts
+++ Sinwy.Shared/types/user/User.ts
@@ -1,4 +1,4 @@
 export type User = {
-    name: string;
-    id: number;
-}
+	name: string;
+	id: number;
+};
